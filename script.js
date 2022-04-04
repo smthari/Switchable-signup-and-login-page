174337ccd535612234a1b5586b1c3edd580fd0fc
Sumit Harijan <smtharijan@gmail.com>
2022-04-04 11:12:05 +0530

    final commit

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ function openForm(evt, formName) {
   // Declare all variables
   var i, switchcontent, switchlinks;
 
-  // Get all elements with class="switchcontent" and hide them
+  // Get all elements with class="switchcontent" and hide them using for loop
   switchcontent = document.getElementsByClassName("switchcontent");
   for (i = 0; i < switchcontent.length; i++) {
     switchcontent[i].style.display = "none";
   }
 
-  // Get all button elements with class="switchlink" and remove the class "active"
+  // Get all button elements with class="switchlink" and remove the class "active" for loop
   switchlinks = document.getElementsByClassName("switchlink");
   for (i = 0; i < switchlinks.length; i++) {
     switchlinks[i].className = switchlinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(formName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 // show default form
 document.getElementById("defaultOpen").click();
+
+// show form manually based on fornName
+// document.getElementById("LogIn").style.display = "block";
